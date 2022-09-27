b95919382c4f627f27b2d099f9f37b8f7a28bd7f
M Starch <LeStarch@googlemail.com>
2022-09-26 17:53:01 -0700

    lestarch: rearranging former Arduino packages

--- fprime-arduino.cmake
+++ fprime-arduino.cmake
@@ -1,10 +1,10 @@
 # Disable targets not supported on Arduino
-#set_target_properties(
-#    Svc_LinuxTime
-#    Drv_SocketIpDriver
-#    PROPERTIES EXCLUDE_FROM_ALL 1 EXCLUDE_FROM_DEFAULT_BUILD 1)
-add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/fprime-arduino/ArduinoOs")
-add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/fprime-arduino/ArduinoTime")
-add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/fprime-arduino/ArduinoDrv/SerialDriver")
-add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/fprime-arduino/ArduinoDrv/HardwareRateDriver")
+set_target_properties(
+    Svc_LinuxTime
+    Drv_SocketIpDriver
+    PROPERTIES EXCLUDE_FROM_ALL 1 EXCLUDE_FROM_DEFAULT_BUILD 1)
+add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Arduino/Os")
+add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Arduino/ArduinoTime")
+add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Arduino/Drv/SerialDriver")
+add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Arduino/Drv/HardwareRateDriver")
 
