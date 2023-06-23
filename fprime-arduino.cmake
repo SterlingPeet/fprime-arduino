b877aae7d0651f60e1c6d1ae98ac69f7e4a31bc8
Ethan Chee <ethancheez@gmail.com>
2023-06-23 15:52:17 -0700

    esp32 support

--- fprime-arduino.cmake
+++ fprime-arduino.cmake
@@ -1,11 +1,16 @@
 starts_with(IS_MBED "${ARDUINO_FQBN}" "arduino:mbed")
+starts_with(IS_ESP32 "${ARDUINO_FQBN}" "esp32")
+
 if (IS_MBED)
     set(CMAKE_CXX_STANDARD 14)
     add_compile_definitions(_GNU_SOURCE)
 endif()
+if (IS_ESP32)
+    set(CMAKE_CXX_STANDARD 20)
+endif()
+
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Arduino/Os")
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Arduino/ArduinoTime")
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Arduino/Drv/GpioDriver")
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Arduino/Drv/StreamDriver")
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Arduino/Drv/HardwareRateDriver")
-
